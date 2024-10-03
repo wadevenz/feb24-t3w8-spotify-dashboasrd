71d10197d5b76f317316dcaaee201b569837687a
Wade <158378370+wadevenz@users.noreply.github.com>
2024-10-03 21:12:56 +1000

    feat: signin with spotify works

--- src/contexts/SpotifyAuthProvider.jsx
+++ src/contexts/SpotifyAuthProvider.jsx
@@ -43,7 +43,7 @@ export function SpotifyAuthProvider({children}){
             setUserAuthData(authData);
             // This cleans up the URL in the browser tab
             // removing the Spotify auth data so it doenst affect the page load useEffect
-            window.history.replace(null, "Spotify Statsboard", "/");
+            window.history.replaceState(null, "Spotify Statsboard", "/");
         }
         if (userAuthCode){
             getAuthData();
@@ -74,8 +74,47 @@ export function SpotifyAuthProvider({children}){
 
     }
 
+
+    // This is the one that sends users to Spotify
+    async function redirectToAuthCodeFlow() {
+        const verifier = generateCodeVerifier(128);
+        const challenge = await generateCodeChallenge(verifier);
+    
+        localStorage.setItem("verifier", verifier);
+    
+        const params = new URLSearchParams();
+        params.append("client_id", clientId);
+        params.append("response_type", "code");
+        params.append("redirect_uri", "http://localhost:5173/spotifycallback");
+        params.append("scope", "user-read-private user-read-email");
+        params.append("code_challenge_method", "S256");
+        params.append("code_challenge", challenge);
+    
+        document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    }
+    
+    function generateCodeVerifier(length) {
+        let text = '';
+        let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    
+        for (let i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+    }
+    
+    async function generateCodeChallenge(codeVerifier) {
+        const data = new TextEncoder().encode(codeVerifier);
+        const digest = await window.crypto.subtle.digest('SHA-256', data);
+        return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+            .replace(/\+/g, '-')
+            .replace(/\//g, '_')
+            .replace(/=+$/, '');
+    }
+
+
     return (
-        <SpotifyAuthContext.Provider value = {{userAuthData}}>
+        <SpotifyAuthContext.Provider value = {{userAuthData, redirectToAuthCodeFlow}}>
             {children}
         </SpotifyAuthContext.Provider>
     )
