1b6f72f3b8a77fb1f34741d204ecff26f03ea206
Wade <158378370+wadevenz@users.noreply.github.com>
2024-10-05 11:10:40 +1000

    feat: fetch top tracks

--- src/contexts/SpotifyProfileProvider.jsx
+++ src/contexts/SpotifyProfileProvider.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
+import { useSpotifyAuthContext } from "./SpotifyAuthProvider";
 
 
 export const defaultProfileData = {
@@ -20,8 +21,59 @@ export function useSpotifyProfileData(){
 
 export function SpotifyProfileProvider({children}){
 
+    let [profileData, setProfileData] = useState(defaultProfileData);
+    let [topTracks, setTopTracks] = useState({})
+
+    // Access auth data from auth provider so that we can make more fetch requests
+    let {userAuthData} = useSpotifyAuthContext();
+
+    async function fetchProfileData(accessToken){
+        const result = await fetch(
+            "https://api.spotify.com/v1/me",
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            }
+        );
+
+        return await result.json(); 
+    }
+
+    async function fetchTopTracks(accessToken){
+        const result = await fetch(
+            "https://api.spotify.com/v1/me/top/tracks",
+            {
+                method: "GET",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            }
+        );
+
+        return await result.json(); 
+    }
+
+
+    useEffect(() => {
+
+        // if authdata has an access token, start making fetch requests
+        if (userAuthData && userAuthData.access_token){
+            fetchProfileData(userAuthData.access_token).then(profileData => {
+                setProfileData(profileData);
+            });
+            fetchTopTracks(userAuthData.access_token).then(result => {
+                setTopTracks(result);
+            });
+        }
+
+        // whenever auth data changes, check it and maybe fetch request happens
+    }, [userAuthData]);
+
+
     return (
-        <SpotifyProfileContext.Provider>
+        <SpotifyProfileContext.Provider value = {{profileData, topTracks}}>
             {children}
         </SpotifyProfileContext.Provider>
     )
