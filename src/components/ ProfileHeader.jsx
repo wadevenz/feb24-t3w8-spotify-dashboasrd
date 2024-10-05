170131f2444a2db87cec4d9696269fb3fe0fad4d
Wade <158378370+wadevenz@users.noreply.github.com>
2024-10-05 11:45:34 +1000

    feat: render multiple artists per track

--- src/components/ ProfileHeader.jsx
+++ src/components/ ProfileHeader.jsx
@@ -7,10 +7,12 @@ export function ProfileHeader(){
 
     if (profileData.id){
         return <div id="profileHeader">
-            <h1>Spotify Stats for {profileData.display_name}</h1>
-            {profileData.images.length > 0 && <img src={profileData.images[0].url} />}
-            
-        </div>
+                    <h2 id="title">Spotify Stats for </h2>
+                    {profileData.images.length > 0 && (
+                    <img src={profileData.images[1].url} />
+                    )}
+                    <h1 id="name">{profileData.display_name}</h1>
+            </div>
     } else {
        return <div id="profileHeader">
             <p>Please sign in to see your profile data</p>
